00afbe527fb85220230f0dd7fcc1995e41b15b14
Simone Carriero <simonecarriero95@hotmail.com>
2022-09-21 13:31:20 +0200

    Create search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.scss'
 import Header from './components/header/Header'
+import Search from './components/search/Search'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   )
 }
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,13 @@
+import './Search.scss'
+
+export const Search = () => {
+  return (
+    <div className='search'>
+      <input
+        className='search-input'
+        type="search"
+        placeholder='Search...'
+      />
+    </div>
+  )
+}
